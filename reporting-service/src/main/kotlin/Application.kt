8daf65d0dfc4d1f33e2d5c84b175e95567ca981b
Alexander Naumov <anaumov@solutionmentors.com>
2020-12-13 20:07:42 +0200

    reporting-service: missed code added.

--- reporting-service/src/main/kotlin/Application.kt
+++ reporting-service/src/main/kotlin/Application.kt
@@ -1,35 +1,47 @@
+import deserialize.TransactionDeserializer
 import model.Transaction
 import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringSerializer
+import java.time.Duration
+import java.util.*
 
 private const val VALID_TRANSACTIONS_TOPIC = "valid-transactions"
 private const val SUSPICIOUS_TRANSACTIONS_TOPIC = "suspicious-transactions"
 private const val BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094"
 
 fun main() {
-    /**
-     *         String consumerGroup = /** Decide on the name for the consumer group.**/
-
-    System.out.println("Consumer is part of consumer group " + consumerGroup);
-
-    Consumer<String, Transaction> kafkaConsumer = createKafkaConsumer(BOOTSTRAP_SERVERS, consumerGroup);
-
-    consumeMessages(Collections.unmodifiableList(Arrays.asList(SUSPICIOUS_TRANSACTIONS_TOPIC, VALID_TRANSACTIONS_TOPIC)), kafkaConsumer);
-     */
+    val consumerGroup = "reporting-service"
+    println("Consumer is part of consumer group $consumerGroup")
+    val kafkaConsumer = createKafkaConsumer(BOOTSTRAP_SERVERS, consumerGroup)
+    consumeMessages(listOf(SUSPICIOUS_TRANSACTIONS_TOPIC, VALID_TRANSACTIONS_TOPIC), kafkaConsumer)
 }
 
 private fun createKafkaConsumer(bootstrapServers: String, consumerGroup: String): Consumer<String, Transaction> {
-    /**
-     * Fill in the code here to subscribe to the provided topics
-     * Run in a loop and consume all the transactions
-     * Record the transactions for reporting based on the topic
-     */
+    val props = Properties()
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, TransactionDeserializer::class.java.name)
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup)
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+    return KafkaConsumer(props)
 }
 
 private fun consumeMessages(topics: List<String>, kafkaConsumer: Consumer<String, Transaction>):Unit {
-    /**
-     * Configure all the Kafka client parameters here
-     * Create and return new Kafka consumer
-     */
+    kafkaConsumer.subscribe(topics)
+    while (true) {
+        val consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1))
+        if (consumerRecords.isEmpty) {
+            continue
+        }
+        consumerRecords.forEach {
+            println("Received record (key: ${it.key()}, value: ${it.value()}, " +
+                    "partition: ${it.partition()}, offset: ${it.offset()} from topic ${it.topic()}")
+            recordTransactionForReporting(it.topic(), it.value())
+        }
+        kafkaConsumer.commitAsync()
+    }
 }
 
 private fun recordTransactionForReporting(topic: String, transaction: Transaction):Unit {
